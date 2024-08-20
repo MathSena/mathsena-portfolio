201ab0c9ee56559a47aeb243bd1b0bb62d2964f7
MathSena <MathSena07@hotmail.com>
2024-08-20 23:13:14 +0100

    chore: Update home page layout and add Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
+import { Home } from './pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Mathsena</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
+    <Home />
   )
 }
 
---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,60 @@
+import { Box, Container, Grid, Typography, Button, styled } from "@mui/material";
+import Avatar from "../../../../assets/images/avatar.jpg";
+
+const Hero = () => {
+
+    const StyledHero = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        [theme.breakpoints.up('xs')]: { // <= mobile
+            paddingTop: "100px",
+        },
+        [theme.breakpoints.up('md')]: { // >=mobile
+            paddingTop: "0",
+        }
+    }));
+
+    const StyledImg = styled("img")(({ theme }) => ({
+        width: "75%",
+        borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
+    }));
+
+    return (
+        <>
+            <StyledHero>
+                <Container maxWidth="lg">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={5}>
+                            <Box position="relative">
+                                <Box position="relative" textAlign="center">
+                                    <StyledImg src={Avatar} />
+                                </Box>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={7}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Matheus Sena</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
+                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                    <Button variant="contained" color="secondary">
+                                        Download CV
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                    <Button variant="contained" color="secondary">
+                                        Contact me
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </StyledHero>
+        </>
+    );
+}
+
+export default Hero;
